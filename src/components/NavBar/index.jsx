ded2cc9b028e1c77f2905745e0f5e02822859acf
rcesonis <rimvydas.cesonis@gmail.com>
2022-03-21 17:39:25 +0100

    Deleted navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,18 +1,11 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/shop">Shop</Nav.Link>
-            <Nav.Link href="/details/1">Details</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <p>
+        <Link to="/">Home</Link>
+      </p>
     </div>
   );
 };
