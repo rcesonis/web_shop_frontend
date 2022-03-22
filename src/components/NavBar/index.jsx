37124f64451c9e2e1e2e6427d05fa8424adcae76
rcesonis <rimvydas.cesonis@gmail.com>
2022-03-22 10:52:08 +0100

    Created a Navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,11 +1,35 @@
+import React from "react";
+import "./NavBar.scss";
 import { Link } from "react-router-dom";
+import logo2 from "../../styles/logo2.png";
 
 export const NavBar = () => {
   return (
-    <div>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-    </div>
+    <nav>
+      <div className="container">
+        <img className="logo" src={logo2} />
+        <div className="menu">
+          <Link to="/">Home</Link>
+          <Link to="/shop">Shop</Link>
+          <input type="search" id="site-search"></input>
+        </div>
+        <div className="menu-icons">
+          <Link to="/">
+            <i class="las la-user"></i>
+          </Link>
+          <Link to="/">
+            <i class="las la-heart"></i>
+          </Link>
+          <Link to="/">
+            <i class="las la-shopping-cart"></i>
+          </Link>
+        </div>
+        <button className="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+    </nav>
   );
 };
