e6ac017ac4e5403200da39d7647c351821268f88
georgios63 <georgemilitsopoulos63@gmail.com>
2022-03-25 12:04:31 +0100

    some basic styling for homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
     <div className="home-container">
       <div className="home-links">
         <h1>WORK IN PROGRESS</h1>
+        <p>This page doesn't exist!</p>
+        <p>
+          Please navigate to our{" "}
+          <Link
+            to={{
+              pathname: "/shop",
+            }}
+          >
+            SHOP PAGE
+          </Link>
+        </p>
       </div>
     </div>
   );
